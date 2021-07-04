919919d24ef10cdaef3e8d39c8fd4d4dcda0b42f
xrasel81 <raselsheikh123@gmail.com>
2021-07-04 22:01:25 +0600

    fadeIn Toogle allert

--- js/main.js
+++ js/main.js
@@ -11,4 +11,19 @@ $('.hide_show').click(function(){
     $('.nav').toggle('slow');
 });
 
+$('.fade-color').click(function(){
+    $('.red').fadeIn('slow');
+    $('.green').fadeIn(2000);
+    $('.blue').fadeIn(3000);
+    
+});
+$('.fade-toggle').click(function(){
+    $('.red').fadeToggle(1000);
+    $('.green').fadeToggle(2000);
+    $('.blue').fadeToggle(3000);
+});
+$('.alart-link').click(function(){
+    alert("Are you Sure To Open This Page");
+});
+
 });
